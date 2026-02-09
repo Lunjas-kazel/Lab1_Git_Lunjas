deb1e004edd967256f0ab652c254ffaacc617263
Kazel Lunjas <lunjaskazelimperial@gmail.com>
2026-02-09 18:05:27 +0800

    Added section info for Lunjas.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Lab 1: Version Control")
 print("Author: [Kazel Lunjas]")
 print("Student ID: TUPM-25-0495")
+print("Lab 1: Version Control")
+print("Author: Kazel Lunjas")
+print("Student ID: TUPM-25-0495")
+print("Class Section: [BSME 1B]")
